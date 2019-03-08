7726532a1128425bee7c0866d5e16459cf4bb0af
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-08 21:37:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
 function basicTeenager(age) {
-  if(age <= 19 && age => 13) {
+  if(age < 19 && age > 13) {
     return "You are a teenager!";
   } else {
     return undefined;
@@ -7,7 +7,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if(age <= 19 && age => 13) {
+  if(age < 19 && age > 13) {
     return "You are a teenager!";
   } else {
     return "You are not a teenager";
